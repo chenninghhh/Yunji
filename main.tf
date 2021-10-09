00f4bba0caa7d322390ced4b9931f8c0eb8a0487
chenninghhh <92013381+chenninghhh@users.noreply.github.com>
2021-10-09 10:40:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
+#指定服务提供者
 provider "alicloud" {
 }
-
+#创建安全组
 resource "alicloud_security_group" "group" {
   name        = "tf_test_foo"
   description = "foo"
   vpc_id      = alicloud_vpc.vpc.id
 }
-
+#创建一个vpc专用网络
 resource "alicloud_vpc" "vpc" {
   name       = "test_vpc"
   cidr_block = "172.16.0.0/16"
 }
-
+#虚拟交换机
 resource "alicloud_vswitch" "vswitch" {
   vpc_id            = alicloud_vpc.vpc.id
   cidr_block        = "172.16.0.0/24"
   vswitch_name      = "test_vpc"
   availability_zone = "cn-beijing-b"
 }
-
+#创建ECS实例
 resource "alicloud_instance" "instance" {
   # cn-beijing
   availability_zone = "cn-beijing-b"
